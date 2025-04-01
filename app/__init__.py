dc4ba98b838c8c6ae58521014dea1ec97c608ca7
Kevin Lin <klin50@stuy.edu>
2025-04-01 11:01:37 -0400

    added basic functions

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+import sqlite3
+from flask import Flask,render_template,request,session,redirect,url_for,flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+@app.route("/visualization")
+def visual():
+
+#Checks if user info is the same as the one saved in database
+@app.route("/login")
+def login():
+
+#Adds user info to database
+@app.route("/register")
+def register():
+
+
+@app.route("/preferences")
+def preferences():
+
+@app.route("/interest")
+def interest():

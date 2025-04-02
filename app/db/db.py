1644468b4a30f121cdea5da9f7d311c0e013da12
Tawab <angrybirdsrock2007@gmail.com>
2025-04-02 11:01:48 -0400

    added db funcs

---
+++ app/db/db.py
@@ -0,0 +1,51 @@
+'''
+Shadow Devo Money Gang - Danny Mok, Colyi Chen, Kevin Liu, Tawab Berri
+SoftDev
+Spring 2025
+p04 - Trump Insults
+'''
+
+import sqlite3
+
+DB_FILE = "database.db"
+
+db = sqlite3.connect(DB_FILE, check_same_thread = False)
+c = db.cursor()
+
+f = open("app/static/trump_insult_tweets_2014_to_2021.csv", "r")
+print(f.read())
+
+def create():
+    c.execute("DROP TABLE IF EXISTS users;")
+    c.execute(
+        '''CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL,
+                target_pref TEXT,
+                insult TEXT
+                );
+           ''')
+
+    c.execute(
+        '''CREATE TABLE IF NOT EXISTS csv (
+            number INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            target TEXT NOT NULL,
+            insult TEXT NOT NULL,
+            tweet TEXT NOT NULL
+        );
+            ''')
+    db.commit()
+
+def createU(name, passw):
+    c.execute("INSERT INTO users (username, password) VALUES (?, ?);", (name, passw))
+    db.commit()
+    print(f"added user {name} to database!")
+
+def getUserPass(id):
+    c.execute("SELECT username, password FROM users WHERE id = ?", (id,))
+    row = c.fetchone()
+    if row == None:
+        return None
+    return [row[0], row[1]]

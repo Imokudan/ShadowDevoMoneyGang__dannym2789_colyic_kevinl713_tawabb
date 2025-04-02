ed109f543295ab19883454b32070ab54c803cefb
Danny Mok <144475495+Imokudan@users.noreply.github.com>
2025-04-02 09:21:10 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask,render_template,request,session,redirect,url_for,flash
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def home():
@@ -9,18 +10,26 @@ def home():
 
 @app.route("/visualization")
 def visual():
+  return render_template('home.html')
 
 #Checks if user info is the same as the one saved in database
 @app.route("/login")
 def login():
+  return render_template('home.html')
 
 #Adds user info to database
 @app.route("/register")
 def register():
-
+  return render_template('home.html')
 
 @app.route("/preferences")
 def preferences():
+  return render_template('home.html')
 
 @app.route("/interest")
 def interest():
+  return render_template('home.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(use_reloader=False, debug=False)

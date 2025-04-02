886d9783bbf797a3feb4dd55ac93c5bdb0c85f12
Kevin Lin <klin50@stuy.edu>
2025-04-02 10:54:49 -0400

    added login to flask

--- app/__init__.py
+++ app/__init__.py
@@ -7,29 +7,43 @@ app.secret_key = os.urandom(32)
 
 @app.route("/")
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
 
 @app.route("/visualization")
 def visual():
-  return render_template('home.html')
+    #Check login
+    if 'username' not in session:
+        return redirect('/')
+    return render_template('home.html')
 
 #Checks if user info is the same as the one saved in database
 @app.route("/login")
 def login():
-  return render_template('home.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    #Check if user and pass match database
+        #session['username'] = username
+    return render_template('login.html')
 
 #Adds user info to database
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-  return render_template('home.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    #Check if username is the same otherwise add to database
+    return render_template('register.html')
 
 @app.route("/preferences")
 def preferences():
-  return render_template('home.html')
+    if 'username' not in session:
+        return redirect('/')
+    return render_template('home.html')
 
 @app.route("/interest")
 def interest():
-  return render_template('home.html')
+    if 'username' not in session:
+        return redirect('/')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.debug = True
